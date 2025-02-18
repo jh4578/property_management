f93ec07403ebffddb2eca2a9808ce2d0126a4dd4
jh4578 <116768893+jh4578@users.noreply.github.com>
2025-02-18 18:57:02 -0500

    Update search.py

--- search.py
+++ search.py
@@ -220,7 +220,7 @@ def app():
         grid_response = AgGrid(
             df, 
             gridOptions=grid_options,
-            height=1000, 
+            height=600, 
             width='100%',
             data_return_mode='AS_INPUT', 
             update_mode='MODEL_CHANGED',
